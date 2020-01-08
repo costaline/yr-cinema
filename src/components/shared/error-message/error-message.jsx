a24be37adbc0c257ba1846769703edf9a87c87d8
Constantine Zubarev <kv.zubarev@gmail.com>
2020-01-08 16:09:33 +0600

    #2: add shared components

---
+++ src/components/shared/error-message/error-message.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const ErrorMessage = () => (
+  <>
+    <h1>Something went wrong...</h1>
+  </>
+);
---
+++ src/components/shared/loader/loader.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Loader = () => {
+  return <h2>Loading...</h2>;
+};
