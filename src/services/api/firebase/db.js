445a68f6d2831f0914f965c6a01bb5672994924e
Constantine Zubarev <kv.zubarev@gmail.com>
2020-01-11 12:51:57 +0600

    #2: refactor firebase db get

--- src/services/api/firebase/db.js
+++ src/services/api/firebase/db.js
@@ -1,38 +1,83 @@
 import axios from 'axios';
+import queryString from 'querystring';
 
 const firebase = axios.create({
   baseURL: 'https://yr-cinema.firebaseio.com/'
 });
 
 export const db = {
-  /**
-   * Receiving data from the server
-   * @param {string} resource - requested resource in the singular
-   * @returns {Array}
-   */
-  getResource: async (resource) => {
-    try {
-      const response = await firebase.get(`${resource}s.json`);
-
-      const data = transformResponseData(response.data, resource);
-      return data;
-    } catch (err) {
-      throw err.message;
-    }
-  }
+  // имитация получение ресурса по частям
+  getResource: (resource, currentPage) => getResource(resource, currentPage)
 };
 
-/**
- * Transformation of data from an object into an array with an id in the form of a resource value
- * @param {Object} data - data after response
- * @param {string} resource - name used for dynamic id
- * @returns {Array} - an array of data objects
- */
-const transformResponseData = (data, resource) => {
-  const transformData = Object.keys(data).map((key) => ({
-    [`${resource}Id`]: key,
-    ...data[key]
-  }));
+// TODO: перенести в настройки пользователя: limit, sortDirection
+// количество элементов для загрузки
+const limit = 5;
+// направление сортировки массива по timestamp
+const sortDirection = 1;
 
-  return transformData;
+// имитация получение ресурса по частям
+const getResource = async (resource, currentPage = 1) => {
+  // получаем ВСЕ данные о ресурсе с сервера
+  const response = await firebase.get(`${resource}s.json`);
+
+  // преобразуем в массив объектов
+  const transformedData = transformData(response.data, resource);
+
+  // сортируем по timestamp
+  const sortedData = sortData(transformedData, sortDirection);
+
+  // определяем количество элементов
+  const total = sortedData.length;
+
+  // определяем количество страниц
+  const pagesCount = Math.ceil(total / limit);
+
+  // заполняем массив страниц
+  const pages = range(1, pagesCount);
+
+  // получаем начальную точку загрузки
+  const startValue = sortedData[(currentPage - 1) * limit].timestamp;
+
+  // формируем запрос
+  const queryData = {
+    [sortDirection === 1 ? 'limitToFirst' : 'limitToLast']: limit,
+    [sortDirection === 1 ? 'startAt' : 'endAt']: startValue
+  };
+  const query = queryString.stringify(queryData);
+
+  // получаем часть данных о ресурсе с сервера
+  const chunkResponse = await firebase.get(
+    `${resource}s.json/?orderBy="timestamp"&${query}`
+  );
+
+  // преобразуем в массив объектов
+  const transformedChunkData = transformData(chunkResponse.data, resource);
+
+  // сортируем по timestamp
+  const sortedChunkData = sortData(transformedChunkData, sortDirection);
+  console.log('sortedChunkData: ', sortedChunkData);
+
+  return sortedChunkData;
+};
+
+// дополнительные функции -----------------------------------------------
+// трансформация объекта объектов в массив объектов c id
+const transformData = (data, resource) => {
+  return Object.keys(data).map((key) => {
+    return {
+      [`${resource}Id`]: key,
+      ...data[key]
+    };
+  });
+};
+
+// сортировка массива по timestamp
+const sortData = (data, sortDirection = 1) => {
+  return data.sort((a, b) => (a.timestamp - b.timestamp) * sortDirection);
+};
+
+// заполнение массива страниц
+const range = (start, end) => {
+  return [...Array(end).keys()].map((el) => el + start);
 };
