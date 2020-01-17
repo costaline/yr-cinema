8212461e84b7f43fbbcafa7e4c79e867762263f1
Constantine Zubarev <kv.zubarev@gmail.com>
2020-01-17 11:49:52 +0600

    #3: update test home page

--- src/routes/home-page/__tests__/home-page.test.js
+++ src/routes/home-page/__tests__/home-page.test.js
@@ -3,8 +3,11 @@ import { shallow } from 'enzyme';
 
 import { HomePage } from '../home-page';
 import { Loader } from '~components/shared/loader';
+import { ErrorMessage } from '~components/shared/error-message';
+import { FilmsList } from '~components/home-page/films-list';
+import Pagination from '~components/shared/pagination/pagination';
 
-describe('Home page', () => {
+describe('Home Page with...', () => {
   const props = {
     fetchFilms: () => {},
     films: [],
@@ -14,7 +17,7 @@ describe('Home page', () => {
     location: {}
   };
 
-  it('Loading', () => {
+  describe('Loader', () => {
     const nextProps = {
       ...props,
       loading: true
@@ -22,6 +25,63 @@ describe('Home page', () => {
 
     const wrapper = shallow(<HomePage {...nextProps} />);
 
-    expect(wrapper.contains(<Loader />)).toEqual(true);
+    it('render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('contain', () => {
+      expect(wrapper.contains(<Loader />)).toEqual(true);
+    });
+  });
+
+  describe('Error message', () => {
+    const nextProps = {
+      ...props,
+      error: 'Error message'
+    };
+
+    const wrapper = shallow(<HomePage {...nextProps} />);
+
+    it('render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('contain', () => {
+      expect(wrapper.contains(<ErrorMessage />)).toEqual(true);
+    });
+  });
+
+  describe('Films list', () => {
+    const nextProps = {
+      ...props,
+      films: [{ name: 'name1' }, { name: 'name1' }]
+    };
+
+    const wrapper = shallow(<HomePage {...nextProps} />);
+
+    it('render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('contain', () => {
+      expect(wrapper.find(FilmsList)).toHaveLength(1);
+    });
+  });
+
+  describe('Pagination', () => {
+    const nextProps = {
+      ...props,
+      total: 3
+    };
+
+    const wrapper = shallow(<HomePage {...nextProps} />);
+
+    it('render correctly', () => {
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('contain', () => {
+      expect(wrapper.find(Pagination)).toHaveLength(1);
+    });
   });
 });
