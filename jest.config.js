f8246171b74af3ba3bfa960882fed19c9d5a9f5c
Constantine Zubarev <kv.zubarev@gmail.com>
2020-01-17 11:49:27 +0600

    #3: update config

--- jest.config.js
+++ jest.config.js
@@ -8,8 +8,9 @@ module.exports = {
 
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
-    // '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js',
-    '\\.(css|scss)$': 'identity-obj-proxy'
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+    '\\.(css|scss)$': '<rootDir>/__mocks__/styleMock.js'
   },
-  setupFilesAfterEnv: ['<rootDir>/setupTests.js']
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer']
 };
