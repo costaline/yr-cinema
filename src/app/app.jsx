89721ecde10fdf12fdad1aded1438758ca0898b8
Constantine Zubarev <kv.zubarev@gmail.com>
2020-01-31 14:25:58 +0600

    #4: add initialized behavior to app

--- src/app/app.jsx
+++ src/app/app.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 import NavBar from '~components/navbar';
 import Routes from '~routes';
+import { getIsInitialized } from '~store/app/selectors';
+import { initializedUser } from '~store/app/actions';
+import { Loader } from '~components/shared/loader';
+import styled from 'styled-components';
 
 import './app.scss';
 
-const App = () => {
+const App = ({ isInitialized, initializeApp }) => {
+  useEffect(() => {
+    initializeApp();
+  }, [initializeApp]);
+
+  if (!isInitialized) {
+    return (
+      <StyledInitial>
+        <Loader />
+      </StyledInitial>
+    );
+  }
+
   return (
     <>
       <NavBar />
@@ -14,4 +32,15 @@ const App = () => {
   );
 };
 
-export default App;
+const mapStateToProps = (state) => ({
+  isInitialized: getIsInitialized(state)
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ initializeApp: initializedUser }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+const StyledInitial = styled.div`
+  height: 100vh;
+`;
