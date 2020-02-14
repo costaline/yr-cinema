97b88c7c8c688abb9e9a65103b78779216a4dd9d
Constantine Zubarev <kv.zubarev@gmail.com>
2020-02-14 12:12:22 +0600

    v0.3.0: update config for snapshots tests

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,13 @@
 module.exports = {
   moduleNameMapper: {
+    // '^~src(.*)$': '<rootDir>/src$1',
+    // '^~public(.*)$': '<rootDir>/public$1',
     '^~components(.*)$': '<rootDir>/src/components$1',
     '^~hocs(.*)$': '<rootDir>/src/hocs$1',
+    '^~routes(.*)$': '<rootDir>/src/routes$1',
+    '^~services(.*)$': '<rootDir>/src/services$1',
     '^~store(.*)$': '<rootDir>/src/store$1',
     '^~utils(.*)$': '<rootDir>/src/utils$1',
-    '^~services(.*)$': '<rootDir>/src/services$1',
 
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/__mocks__/fileMock.js',
